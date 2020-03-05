0b255ac9d0c96e185f9c9d2288d0b7dd2b225fc8
kenny <kenny.lozeau@gmail.com>
2020-03-05 15:15:13 -0500

    Updates

---
+++ precache-manifest.28a202d777386bb20aea8b9962171279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724d6c6e118faaa3c6f871bfa53bf6f9",
+    "url": "/top-wines/index.html"
+  },
+  {
+    "revision": "ea8d6100fbe5f1b78e41",
+    "url": "/top-wines/static/css/main.1f2b37e0.chunk.css"
+  },
+  {
+    "revision": "e61fae664d92b08ca1c9",
+    "url": "/top-wines/static/js/2.9db076f3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/top-wines/static/js/2.9db076f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea8d6100fbe5f1b78e41",
+    "url": "/top-wines/static/js/main.acb9c670.chunk.js"
+  },
+  {
+    "revision": "c9533614d9f2beaed95a",
+    "url": "/top-wines/static/js/runtime-main.817ed291.js"
+  }
+]);
